13132c21d48d323bff080b74bb785aa6cdc2c370
Keshav Garg <kggargkg@gmail.com>
2023-07-28 16:36:13 +0530

    added api request

--- train-schedule-system/src/services/trainService.js
+++ train-schedule-system/src/services/trainService.js
@@ -1,11 +1,26 @@
 // src/services/trainService.js
-import axios from 'axios';
+import axios from "axios";
 
-const API_BASE_URL = 'http://20.244.56.144:80/train/trains'; // Replace with the actual API base URL
-const AUTH_TOKEN = 'sAzlpA'; // Replace with the actual authentication token
+const API_BASE_URL = "http://20.244.56.144/train"; 
+var AUTH_TOKEN;
+
+axios.post(`${API_BASE_URL}/auth`, {
+  body: {
+    companyName: "DMRC",
+    clientID: "9a565944-e994-4421-b03c-3f9512a1c388",
+    ownerName: "Keshav Garg",
+    ownerEmail: "keshavgarg3902@gmail.com",
+    rollNo: "07220802720",
+    clientSecret: "SKNVGPOKLVebtrtu",
+  },
+}).then((Response) => {
+    AUTH_TOKEN=Response;
+}, (error) => {
+  console.log(error);
+});
 
 export const getAllTrains = () => {
-  return axios.get(`${API_BASE_URL}`, {
+  return axios.get(`${API_BASE_URL}/trains`, {
     headers: {
       Authorization: `Bearer ${AUTH_TOKEN}`,
     },
@@ -13,7 +28,7 @@ export const getAllTrains = () => {
 };
 
 export const getTrainById = (trainId) => {
-  return axios.get(`${API_BASE_URL}/${trainId}`, {
+  return axios.get(`${API_BASE_URL}/trains/${trainId}`, {
     headers: {
       Authorization: `Bearer ${AUTH_TOKEN}`,
     },
